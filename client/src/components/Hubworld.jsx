4e3773e1e469004f83de6c2de3bfcca4fdd2197c
Mara <mgreco1991@gmail.com>
2018-02-08 11:45:30 -0500

    resolving merge conflicts take three

--- client/src/components/Hubworld.jsx
+++ client/src/components/Hubworld.jsx
@@ -8,18 +8,7 @@ import Col from "../components/Col";
 export class Hubworld extends Component {
 	render() {
 		return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-			<div className="hubworld">
-			<h1 id="go-forest">Go to the forest</h1>
-			<h1 id="go-to-codesrealm">Go to C'ode's Realm</h1>
-			<h1 id="go-to-tavern">Go to the Tavern</h1>
-			<h1 id="go-to-the-castle">Go to the castle</h1>
-			</div>
-		);
-=======
-=======
->>>>>>> 17eac086538fef9a97681b6eb6bcf423b9a497f6
+
 	<div className = "hubworld">
     	<Hero>
       		<h1> Relics and React </h1>
