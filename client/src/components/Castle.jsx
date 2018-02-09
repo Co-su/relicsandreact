aa758205b403db5bd0679f65e3bccf9e74c7edca
Mara <mgreco1991@gmail.com>
2018-02-09 15:01:38 -0500

    more battle logic

--- client/src/components/Castle.jsx
+++ client/src/components/Castle.jsx
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 export class Castle extends Component {
 
 	banishFromCastle = (e) => {
+		
 		alert("You are not high-enough leveled to enter this realm. Begone!");
 	}
+
 	render() {
 		return (
 			<div className="castle" onClick={this.banishFromCastle}>
 			</div>
 		);
+	
 	}
 }
+
 export default Castle;
